888f8faada65a1af9a760c43fef6eb267ef0aac0
shurfrock <shurfrock.kami@gmail.com>
2024-02-22 18:08:27 -0600

    update web page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { getAllCharacters } from './api'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 function App() {
+  // characters state to store request response
   const [characters, setCharacters] = useState([])
+  // store user search query
+  const [searchValue, setSearchValue] = useState(null)
 
+  // do characters fetch here
   useEffect(() => {
+    // make async function that fetches characters and store them in state
     const fetchCharacters = async () => {
       const characters = await getAllCharacters()
       setCharacters(characters.results)
@@ -14,12 +30,65 @@ function App() {
     fetchCharacters().catch(error => console.error(error))
   }, [])
 
+  // filter characters using search query
+  const filteredCharacters = searchValue
+    ? characters.filter(character => character.name.toLowerCase().includes(searchValue.toLowerCase()))
+    : characters 
+
   return (
     <div>
-      {characters.map(character => (
-        <div key={character.id}>{character.name}</div>
-      ))}
-
+      <Card sx={{ minWidth: 800 }}>
+        <CardContent>   
+        <Stack spacing={2} sx={{ width: 300 }}>
+          <TextField
+            label="Busqueda de Personaje"
+            onChange={(event) => {
+              setSearchValue(event.target.value);
+            }}
+          />
+        </Stack>
+          
+        <TableContainer component={Paper} >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 'bold'}}> Nombre </TableCell>
+                <TableCell sx={{ fontWeight: 'bold'}}> Estado </TableCell>
+                <TableCell sx={{ fontWeight: 'bold'}}> Especie </TableCell>
+                <TableCell sx={{ fontWeight: 'bold'}}> Género </TableCell>
+                <TableCell sx={{ fontWeight: 'bold'}}> Origen </TableCell>
+                <TableCell sx={{ fontWeight: 'bold'}}> Link </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredCharacters.map(character => (
+                <TableRow key={character.id}>
+                  
+                  <TableCell>
+                    {character.name}
+                  </TableCell>
+                  <TableCell>
+                    {character.status}
+                  </TableCell>
+                  <TableCell>
+                    {character.species}
+                  </TableCell>
+                  <TableCell>
+                    {character.gender}
+                  </TableCell>
+                  <TableCell>
+                    {character.origin.name}
+                  </TableCell>
+                  <TableCell>
+                      <img src={character.image}  style={{ width: '100px', height: '100px' }}/>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </CardContent>
+      </Card>
 
     </div>
   )
